1c7d4de95a95e7ea23ad66a0466bd4c6bbe2bfa4
Zsolt Tóth <horvathjeno1944@msn.com>
2018-04-14 14:04:42 +0200

    Implemented Chorus Effect.

--- audio/FX/ChorusEffect.cpp
+++ audio/FX/ChorusEffect.cpp
@@ -11,5 +11,65 @@ ALenum ChorusEffect::getEffectType()
 {
 	return AL_EFFECT_CHORUS;
 }
+void ChorusEffect::setIsWaveformTriangle(bool isWaveformTriangle)
+{
+	alEffecti(effectID,AL_CHORUS_WAVEFORM, isWaveformTriangle);
+}
+bool ChorusEffect::getIsWaveformTriangle(void) const
+{
+	int tmp;
+	alGetEffecti(effectID,AL_CHORUS_WAVEFORM,&tmp);
+	return tmp != 0;
+}
+void ChorusEffect::setPhase(int phase)
+{
+	alEffecti(effectID,AL_CHORUS_PHASE, phase);
+}
+int ChorusEffect::getPhase(void) const
+{
+	int tmp;
+	alGetEffecti(effectID,AL_CHORUS_PHASE,&tmp);
+	return tmp;
+}
+void ChorusEffect::setRate(float rate)
+{
+	alEffectf(effectID,AL_CHORUS_RATE, rate);
+}
+float ChorusEffect::getRate(void) const
+{
+	float tmp;
+	alGetEffectf(effectID,AL_CHORUS_RATE,&tmp);
+	return tmp;
+}
+void ChorusEffect::setDepth(float rate)
+{
+	alEffectf(effectID,AL_CHORUS_DEPTH, rate);
+}
+float ChorusEffect::getDepth(void) const
+{
+	float tmp;
+	alGetEffectf(effectID,AL_CHORUS_DEPTH,&tmp);
+	return tmp;
+}
+void ChorusEffect::setFeedback(float rate)
+{
+	alEffectf(effectID,AL_CHORUS_FEEDBACK, rate);
+}
+float ChorusEffect::getFeedback(void) const
+{
+	float tmp;
+	alGetEffectf(effectID,AL_CHORUS_FEEDBACK,&tmp);
+	return tmp;
+}
+void ChorusEffect::setDelay(const STime& rate)
+{
+	alEffectf(effectID, AL_CHORUS_DELAY,rate.getSeconds());
+}
+STime ChorusEffect::getDelay(void) const
+{
+	float tmp;
+	alGetEffectf(effectID, AL_CHORUS_DELAY, &tmp);
+	return STime::asSeconds(tmp);
+}
 
 }
--- audio/FX/ChorusEffect.hpp
+++ audio/FX/ChorusEffect.hpp
@@ -1,6 +1,7 @@
 #ifndef CHORUSEFFECT_HPP
 #define CHORUSEFFECT_HPP
 #include "Effect.hpp"
+#include "../../sys/STime.hpp"
 
 namespace Audio {
 
@@ -9,6 +10,18 @@ class ChorusEffect : public Effect
 public:
 	ChorusEffect();
 	virtual ALenum getEffectType();
+	void setIsWaveformTriangle(bool isWaveformTriangle);
+	bool getIsWaveformTriangle(void) const;
+	void setPhase(int phase);
+	int getPhase(void) const;
+	void setRate(float rate);
+	float getRate(void) const;
+	void setDepth(float rate);
+	float getDepth(void) const;
+	void setFeedback(float rate);
+	float getFeedback(void) const;
+	void setDelay(const STime& rate);
+	STime getDelay(void) const;
 };
 }
 #endif // CHORUSEFFECT_HPP
